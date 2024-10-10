2324760993102b0e014ab8afb1dfe718e9847d59
sang won Yoo (유상원) <rongleader@naver.com>
2024-10-10 23:14:38 +0900

    목록 DI, 콜렉션, Annotation 추가

--- src/main/java/spring/di/Program.java
+++ src/main/java/spring/di/Program.java
@@ -2,11 +2,17 @@ package spring.di;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import spring.di.entity.Exam;
+import spring.di.entity.NewlecExam;
 import spring.di.ui.ExamConsole;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
 //        Exam exam = new NewlecExam();
+//        Exam exam = new NewlecExam(10, 10, 10, 10);
 //        DI : exam 주입
 //        Inline 방식을 사용하다가 Grid 사용한다고 가정 => Grid로 바꾸는 소스코드가 아니라 외부 설정으로 빼야함
 //        Inline : 한줄에 출력
@@ -24,8 +30,19 @@ public class Program {
 //        캐스팅을 해줘야 한다.
 //        ExamConsole console = (ExamConsole) context.getBean("console");
 
+        Exam exam = context.getBean(Exam.class);
+        System.out.println(exam.toString());
 //        ExamConsole 타입에 맞게 반환하도록 함
         ExamConsole console = context.getBean(ExamConsole.class);
         console.print();
+
+//        List<Exam> exams = new ArrayList<>();
+//        List<Exam> exams = (List<Exam>)context.getBean("exams");
+//        exams.add(new NewlecExam(1, 1, 1, 1));
+
+//        for (Exam e : exams) {
+//            System.out.println(e);이 코드가 자동으로 toString() 메서드 호출
+//            System.out.println(e);
+//        }
     }
 }
--- src/main/java/spring/di/ui/InlineExamConsole.java
+++ src/main/java/spring/di/ui/InlineExamConsole.java
@@ -1,5 +1,6 @@
 package spring.di.ui;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import spring.di.entity.Exam;
 
 public class InlineExamConsole implements ExamConsole {
@@ -18,6 +19,10 @@ public class InlineExamConsole implements ExamConsole {
         System.out.printf("total is %d, avg is %f\n", exam.total(), exam.avg());
     }
 
+//    xml 파일에서 bean 객체의 console 객체를 만남.
+//    그러면 이 파일로 와서 Autowired를 찾는다.
+//    setExam을 자동으로 해준다.
+    @Autowired
     @Override
     public void setExam(Exam exam) {
         this.exam = exam;
